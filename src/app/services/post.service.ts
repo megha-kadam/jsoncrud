673da47c716c5b4c083cadfc1d37d085167d2a6c
megha-kadam <meghakadam506@gmail.com>
2026-02-14 11:25:00 +0530

    upgrade project from 14 to 15

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 import { environment } from 'src/environments/environment';
 import { Ipost } from '../models/post';
 
