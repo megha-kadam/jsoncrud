a68f8a8221e7ed74a15a54638524aec52063273e
megha-kadam <meghakadam506@gmail.com>
2026-02-14 14:53:35 +0530

    upgrade project from 18 to 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { PostService } from './services/post.service';
 import { Ipost } from './models/post';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent implements OnInit{
   title = 'jsoncrud';
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -2,9 +2,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Ipost } from 'src/app/models/post';
 
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+    selector: 'app-post',
+    templateUrl: './post.component.html',
+    styleUrls: ['./post.component.scss'],
+    standalone: false
 })
 export class PostComponent implements OnInit {
   @Input() post !: Ipost;
